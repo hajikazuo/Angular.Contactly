1a5d289287e9757e20b7409812a6ffabf0931272
hajikazuo <hajikazuo@outlook.com>
2024-08-29 14:31:42 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Contact } from '../models/contact.model';
+import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    AsyncPipe,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular.Contactly';
+  http = inject(HttpClient);
+
+  contactForm = new FormGroup({
+    name: new FormControl<string>(''),
+    email: new FormControl<string | null>(null),
+    phone: new FormControl<string>(''),
+    favorite: new FormControl<boolean>(false)
+  })
+
+  contacts$ = this.getContacts();
+
+  onFormSubmit() {
+    const addContactRequest = {
+      name: this.contactForm.value.name,
+      email: this.contactForm.value.email,
+      phone: this.contactForm.value.phone,
+      favorite: this.contactForm.value.favorite,
+    }
+
+    this.http.post('https://localhost:7136/api/Contacts', addContactRequest)
+      .subscribe({
+        next: (value) => {
+          console.log(value);
+          this.contacts$ = this.getContacts();
+          this.contactForm.reset();
+        }
+      })
+  }
+
+  onDelete(id: string) {
+    this.http.delete(`https://localhost:7136/api/Contacts/?id=${id}`)
+      .subscribe({
+        next: (value) => {
+          alert('Item deleted');
+          this.contacts$ = this.getContacts();
+        }
+      })
+  }
+
+  private getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>('https://localhost:7136/api/Contacts');
+  }
 }
---
+++ src/models/contact.model.ts
@@ -0,0 +1,8 @@
+export interface Contact {
+    contactId: string;
+    name: string;
+    email: string | null;
+    phone: string;
+    favorite: boolean;  
+  }
+  
